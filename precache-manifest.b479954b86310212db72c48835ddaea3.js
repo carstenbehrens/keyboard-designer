cb612ce62c4dbc521c283cfaefb4ee8f692fdc78
carstenbehrens <carsten.behrens1@googlemail.com>
2018-10-28 15:49:25 +0100

    Updates

---
+++ precache-manifest.b479954b86310212db72c48835ddaea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "232fdd773a2845607211",
+    "url": "/keyboard-designer/static/js/runtime~main.232fdd77.js"
+  },
+  {
+    "revision": "6995f05f4f2606140f89",
+    "url": "/keyboard-designer/static/js/main.6995f05f.chunk.js"
+  },
+  {
+    "revision": "f7da562b8d68235c03b7",
+    "url": "/keyboard-designer/static/js/1.f7da562b.chunk.js"
+  },
+  {
+    "revision": "6995f05f4f2606140f89",
+    "url": "/keyboard-designer/static/css/main.8c808412.chunk.css"
+  },
+  {
+    "revision": "3b9510919fb030ca42b485767b29b1b1",
+    "url": "/keyboard-designer/index.html"
+  }
+];
